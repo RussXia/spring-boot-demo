4a86e666d6590792abaa11f88611a552c1aeee61
RuzzZZ <xzy199400@foxmail.com>
2017-04-05 16:47:49 +0800

    bugfix

---
+++ demo-dal/src/main/java/com/xzy/demo/dal/DemoDalApplication.java
@@ -0,0 +1,15 @@
+package com.xzy.demo.dal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by RuzzZZ on 2017/4/5.
+ */
+@SpringBootApplication
+public class DemoDalApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(DemoDalApplication.class, args);
+    }
+}
